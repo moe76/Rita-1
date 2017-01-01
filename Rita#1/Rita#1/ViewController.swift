535c9c1a54a9b10bf2e7ac5e87c91d5c7100dc48
moe76 <michael.ostergaard@icloud.com>
2017-01-01 13:58:18 +0100

    Working on displaying images in rows...

--- Rita#1/Rita#1/ViewController.swift
+++ Rita#1/Rita#1/ViewController.swift
@@ -8,11 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        
+        
+        
+        }
+        print(pictures)
+        
+
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
     }
 
     override func didReceiveMemoryWarning() {
