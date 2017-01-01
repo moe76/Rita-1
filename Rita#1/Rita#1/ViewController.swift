ba648d696d02bf659c95cdd4d8ac2f09534de007
moe76 <michael.ostergaard@icloud.com>
2017-01-01 13:08:24 +0100

    Initial Commit

---
+++ Rita#1/Rita#1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rita#1
+//
+//  Created by Michael Østergaard on 01/01/2017.
+//  Copyright © 2017 Michael Østergaard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
